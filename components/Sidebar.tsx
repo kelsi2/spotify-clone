fa00a046e41e5ce4329f15ab025285e6984285f0
Kelsi Proulx <kproulx1@mail.ubc.ca>
2022-06-16 10:11:15 -0700

    Add navMenu

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,5 @@
-import NextImage from 'next/image'
+import NextImage from "next/image";
+import NextLink from 'next/link';
 import {
   Box,
   List,
@@ -17,6 +18,24 @@ import {
   MdFavorite,
 } from "react-icons/md";
 
+const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search",
+  },
+  {
+    name: "Your Library",
+    icon: MdLibraryMusic,
+    route: "/library",
+  },
+];
+
 const Sidebar = () => {
   return (
     <Box
@@ -26,9 +45,29 @@ const Sidebar = () => {
       paddingX="5px"
       color="gray"
     >
-      <Box paddingY='20px'>
-        <Box width='120px' marginBottom='20px' paddingX='20px'>
-          <NextImage src='/sidebar_logo.svg' height={60} width={120} />
+      <Box paddingY="20px">
+        <Box width="120px" marginBottom="20px" paddingX="20px">
+          <NextImage src="/sidebar_logo.svg" height={60} width={120} />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </Box>
     </Box>
