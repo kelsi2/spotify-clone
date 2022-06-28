3f0de826b6a43a59f870f89d19b6227a71ac78d6
Kelsi Proulx <kproulx1@mail.ubc.ca>
2022-06-28 16:04:44 -0700

    Setting up GradientLayout

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -1,20 +1,20 @@
-import { Box } from '@chakra-ui/layout'
-import Sidebar from './Sidebar'
+import { Box } from "@chakra-ui/layout";
+import Sidebar from "./Sidebar";
 
 const PlayerLayout = ({ children }) => {
   return (
-    <Box width='100vw' height='100vh'>
-      <Box position='absolute' top='0' width='250px' left='0'>
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
         <Sidebar />
       </Box>
-      <Box marginLeft='250px' marginBottom='100px'>
-        {children}
+      <Box marginLeft="250px" marginBottom="100px">
+        <Box height="calc(100vh - 100px)">{children}</Box>
       </Box>
-      <Box position='absolute' left='0' bottom='0'>
+      <Box position="absolute" left="0" bottom="0">
         player
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default PlayerLayout
+export default PlayerLayout;
